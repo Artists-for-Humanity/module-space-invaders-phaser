04a05bc8536157f38f1fe06c9e6880bf37c68294
Jamie De Los Santos <77027304+itisjamied@users.noreply.github.com>
2022-01-05 16:18:39 -0500

    added create function
    make the background and player visible

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser'; 
+import Phaser from 'phaser';
 
 class GameScene extends Phaser.Scene {
     constructor() {
@@ -8,19 +8,40 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         console.log("preload START");
-        this.load.image('background', new URL('../assets/myAssets/myBackground.png', import.meta.url).href);
-        this.load.image('projectile', new URL('../assets/myAssets/myProjectile.png', import.meta.url).href);
-        this.load.image('enemy', new URL('../assets/myAssets/myEnemy.png', import.meta.url).href);
-        this.load.image('player', new URL('../assets/myAssets/myPlayer.png', import.meta.url).href);
+        this.load.image('background', new URL('../assets/myAssets/myBackground.png',
+            import.meta.url).href);
+        this.load.image('projectile', new URL('../assets/myAssets/myProjectile.png',
+            import.meta.url).href);
+        this.load.image('enemy', new URL('../assets/myAssets/myEnemy.png',
+            import.meta.url).href);
+        this.load.image('player', new URL('../assets/myAssets/myPlayer.png',
+            import.meta.url).href);
         console.log("preload END")
     }
 
     create() {
         console.log("create");
+        // Add images and sprites to Scene
+        this.add.image(config.width / 2, config.height / 2, 'background');
+
+        // Initialize keyboard manager
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.player = this.physics.add.sprite(config.width / 2, 600, 'player');
+        this.player.setCollideWorldBounds(true);
+
     }
 
     update() {
         console.log("update");
+
+    // Assign arrow keys for movement mechanics
+        if (this.cursors.left.isDown) {
+            this.player.x -= 10;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.x += 10;
+        } 
     }
 }
 
@@ -29,7 +50,7 @@ const config = {
     type: Phaser.AUTO,
     width: 960,
     height: 720,
-    
+
     // Add physics, arcade, and scene
     physics: {
         default: 'arcade',
