4fa81a026ec79aaa9cc3e54dd6956a5d4553e5c7
Stephen Cronin <cronin4392@gmail.com>
2022-01-07 10:50:36 -0500

    Break the game menu out into it's own scene

---
+++ src/scripts/GameOverScene.js
@@ -0,0 +1,44 @@
+import Phaser from 'phaser';
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super({key: 'GameOverScene'});
+    }
+
+    preload() {
+        this.load.image('background', new URL('../assets/background.png', import.meta.url).href);
+    }
+
+    create() {
+        this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'background');
+        this.add.text(
+            this.game.config.width / 2,
+            this.game.config.height / 3,
+            'GAME OVER',
+            {
+                fontFamily: 'Avenir Next',
+                fontSize: '64px',
+                fontStyle: 'bold',
+                fill: '#007fff',
+            }
+        ).setOrigin(0.5);
+
+        this.add.text(
+            this.game.config.width / 2,
+            this.game.config.height * (2/3),
+            `YOUR SCORE WAS ${this.registry.get('score')}\n\nPRESS SPACE TO PLAY AGAIN`,
+            {
+                fontFamily: 'Avenir Next',
+                fontSize: '32px',
+                fontStyle: 'bold',
+                fill: '#007fff',
+                align: 'center'
+            }
+        )
+            .setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+            this.scene.start('GameScene');
+        });
+    }
+}
