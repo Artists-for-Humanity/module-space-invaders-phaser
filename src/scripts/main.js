318e5aab0b585cf438128d518c12ca71f0b043fa
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-01-27 18:29:34 -0500

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,9 +12,16 @@ class GameScene extends Phaser.Scene {
         this.cursors;
         this.enemies;
         this.enemySpeed = 150;
-        this.numEnemies = 1;
+        this.numEnemies = 6;
         this.projectileImg;
         this.projectileState = 'ready'; 
+        this.scoreText;
+        this.score = 0;
+        this.gameOverText;
+        this.gameOver = false;
+        this.GoodJobtext;
+        this.Goodjob = false;
+        this.goodjobVar = 0;
     }
 
     preload() {
@@ -36,9 +43,23 @@ class GameScene extends Phaser.Scene {
         this.projectileImg.visible = false; 
         this.enemies = this.physics.add.group();
         this.setEnemies();
-        this.physics.add.collider(this.projectileImg, this.enemies, this.onProjectileHitEnemy, null, this);
-        this.physics.add.overlap(this.player, this.enemies, this.onPlayerHitEnemy, null, this);
-    }
+        this.physics.add.collider(this.player, this.enemies, this.onPlayerHitEnemy, null, this);
+        this.physics.add.overlap(this.projectileImg, this.enemies, this.onProjectileHitEnemy, null, this);
+        this.scoreText = this.add.text(16, 16, 'score: 0', {
+            fontSize: '32px',
+            fill: 'white',
+        });
+        this.gameOverText = this.add.text(config.width / 2, 400, 'Game Over', {
+            fontSize: '64px',
+            fill: 'white',
+        });
+        this.gameOverText.visible = false;
+        this.GoodJobText = this.add.text(config.width / 2, 400, 'Good job!', {
+            fontsize: '64px',
+            fill:'white',
+        });
+        this.GoodJobText.visible = false;
+    }   
 
     update() {
         console.log("update");
@@ -68,42 +89,73 @@ class GameScene extends Phaser.Scene {
                 body.y += 64;
                 }
             });
-
+            if (this.Goodjob == true){
+                this.ShowGoodJobText()
+            }
         }
 
     fireLazer(){
         this.projectileState = 'fire';
         this.projectileImg.visible = true;
+        this.projectileImg.body.enable = true;
         this.projectileImg.x = this.player.x;
         this.projectileImg.y = this.player.y; 
         this.projectileImg.setVelocityY(-250);
     }
     resetLazer(){
-        if(this.projectileState === 'state'){
+
+        if(this.projectileState === 'ready'){
             return;
         }
+
         this.projectileState = 'ready';
         this.projectileImg.setVelocityY(0);
         this.projectileImg.visible = false; 
     }
     setEnemies(){
         for (let i = 0; i < this.numEnemies; i ++){
-            this.enemies.create(Phaser.Math.Between(64,869), Phaser.Math.Between(64,296), 'enemy');
+            this.enemies.create(Phaser.Math.Between(64,869), 
+            Phaser.Math.Between(64,296), 'enemy');
         }
-        this,this.enemies.setVelocityX(this.enemySpeed * -1);
+        this.enemies.setVelocityX(this.enemySpeed * -1);
+        
     }
     onProjectileHitEnemy(projectileImg, enemy) { 
         enemy.disableBody(true, true);
         projectileImg.body.enable = false; 
         this.resetLazer();
+        this.score += 1;
+        this.scoreText.setText(`score: ${this.score}`);
+        this.goodjobVar += 1;
+        if (this.goodjobVar == 6){
+            this.Goodjob = true;
+        }
 
 
     }
     onPlayerHitEnemy(player) {
         this.physics.pause()
         player.setTint(0xff0000);
+        this.gameOver = true;
+        this.showGameOverText();
     }
+    showGameOverText(){
+        this.gameOverText.setOrigin(0.5);
+        this.gameOverText.visible = true;
+        this.enemies.children.iterate((child) => {
+            child.y = config.height * 2;
+        });
+    }
+    
+    ShowGoodJobText(){
+        this.GoodJobText.setOrigin(0.5);
+        this.GoodJobText.visible = true;
+        this.enemies.children.iterate((child) => {
+            child.y = config.height * 2;
+        });
+        
 
+}
 }
 
 const config = {
