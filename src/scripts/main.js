963701851d5966e5f23f2af99a407bd56e721331
kairo8735 <83092308+kairo8735@users.noreply.github.com>
2022-07-07 17:27:23 -0400

    changed assets

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,17 +7,28 @@ class GameScene extends Phaser.Scene {
         //Misc game object declarations
         this.player;
         this.cursors;
+        this.enemies;
+        this.enemySpeed = 150;
+        this.numEnemies = 6;
         // Paintball object declaration
-        this.ballImg;
-        this.ballState = 'ready';
+        this.projectileImg;
+        this.projectileState = 'ready';
+        this.scoreText;
+        this.score = 0;
+        this.gameOverText;
+        this.gameOver = false;
     }
 
     preload() {
         console.log("preload START");
-        this.load.image('background', new URL('../assets/final/background.png', import.meta.url).href);
-        this.load.image('ball', new URL('../assets/final/ball.png', import.meta.url).href);
-        this.load.image('canvas', new URL('../assets/final/canvas.png', import.meta.url).href);
-        this.load.image('spraycan', new URL('../assets/final/spraycan.png', import.meta.url).href);
+        this.load.image('background', new URL('../assets/main/background.png',
+            import.meta.url).href);
+        this.load.image('projectile', new URL('../assets/main/shot.png',
+            import.meta.url).href);
+        this.load.image('enemy', new URL('../assets/main/alien.png',
+            import.meta.url).href);
+        this.load.image('player', new URL('../assets/main/ship.png',
+            import.meta.url).href);
         console.log("preload END")
     }
 
@@ -25,55 +36,117 @@ class GameScene extends Phaser.Scene {
         console.log("create");
         // Add images to Scene
         this.add.image(480, 360, 'background');
-        this.player = this.physics.add.sprite(480, 600, 'spraycan');
+        this.player = this.physics.add.sprite(480, 600, 'player');
         // Initialize keyboard manager
         this.cursors = this.input.keyboard.createCursorKeys();
         // Set world bounds for player
         this.player.setCollideWorldBounds(true);
         // Paintball
-        this.projectileImg = this.physics.add.sprite(-1440, -1920, 'ball')
+        this.projectileImg = this.physics.add.sprite(-1440, -1920, 'projectile')
         this.projectileImg.visible = false;
+        this.enemies = this.physics.add.group();
+        this.enemies.setVelocityX(this.enemySpeed * -1);
+        this.resetEnemies();
+        this.physics.add.collider(this.player, this.enemies, this.onPlayerHitEnemy, null, this);
+        this.physics.overlap(this.projectileImg, this.enemies, this.onProjectileHitEnemy, null, this);
+        this.scoreText = this.add.text(16, 16, 'Score: 0', {
+            fontSize: '32px',
+            fill: '#000',
+        });
+        this.gameOverText = this.add.text(config.width / 2, 400, 'Game Over', {
+            fontSize: '64px',
+            fill: '#000',
+        });
+        this.gameOverText.visible = false;
     }
 
     update() {
         console.log("update");
+        if (this.gameOver) {
+            return;
+        }
+
         if (this.cursors.left.isDown) {
             this.player.x -= 10;
         }
         if (this.cursors.right.isDown) {
             this.player.x += 10;
         }
-        
-        // Fire the ball
-        fireBall() {
-            this.ballState = 'fire';
-            this.ballImg.visible = true;
-            this.ballImg.x = this.player.x;
-            this.ballImg.y = this.player.y;
-            this.ballImg.setVelocityY(-250);
-        }    
-        if(this,this.cursors.space.isDown) {
-                if (this.ballState == 'ready') {
-                    this.fireBall();
-                }
-            }
 
-        // Reset the ball
-        resetBall() {
-            if (this.ballState === 'ready') {
-                return;
+        if (this, this.cursors.space.isDown) {
+            if (this.projectileState == 'ready') {
+                this.fireProjectile();
             }
-            this.ballState = 'ready';
-            this.ballImg.setVelocityY(0);
-            this.ballImg.visible = false;
         }
 
         // Paintball out of bounds
-        if (this.ballImg).y <= -this.ballImg.height /2 {
-            this.resetBall();
+        if (this.projectileImg.y <= -this.projectileImg.height / 2) {
+            this.resetProjectile();
         }
-        
+
+        this.enemies.children.iterate((child) => {
+            const body = child.body;
+
+            if (body.x < 0) {
+                body.setVelocityX(this.enemySpeed);
+                body.y += 64;
+            } else if (body.x > 896) {
+                    body.setVelocityX(this.enemySpeed * -1);
+                    body.y += 64;
+            }
+        });
+
     }
+    // Reset the ball
+    resetProjectile() {
+        if (this.projectileState === 'ready') {
+            return;
+        }
+        this.projectileState = 'ready';
+        this.projectileImg.setVelocityY(0);
+        this.projectileImg.visible = false;
+    }
+
+    resetEnemies() {
+        for (let i = 0; i < this.numEnemies; i++) {
+            this.enemies.create(Phaser.Math.Between(64, 896), Phaser.Math.Between(64, 296), 'enemy');
+        }
+        this.enemies.setVelocityX(this.enemySpeed * -1);
+    }
+
+    // Fire the ball
+    fireProjectile() {
+        this.projectileState = 'fire';
+        this.projectileImg.visible = true;
+        this.projectileImg.body.enable = true;
+        this.projectileImg.x = this.player.x;
+        this.projectileImg.y = this.player.y;
+        this.projectileImg.setVelocityY(-250);
+    }
+
+    onProjectileHitEnemy(projectileImg, enemy) {
+        enemy.disableBody(true, true);
+        projectileImg.body.enable = false;
+        this.enemy.setTint(0xff0000);
+        this.score += 1;
+        this.scoreText.setText(`Score: ${this.score}`);
+    }
+
+    onPlayerHitEnemy(player) {
+         this.physics.pause();
+         this.player.setTint(0xff0000);
+         this.gamerOver =  true;
+         this.showGameOverText();
+    }
+
+    showGameOverText() {
+        this.gameOverText.setOrigin(0.5);
+        this.gameOverText.visible = true;
+        this.enemies.children.iterate((child) => {
+            child.y = config.height * 2;
+        });
+    }
+
 }
 
 // Set configuration for phaser game instance
