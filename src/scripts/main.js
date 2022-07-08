8dc15b00347f20bf1de30d1159e399d0b1ed5c63
Ctech 21M <ctech21m@Ctech-21Ms-iMac.local>
2022-07-08 14:03:44 -0400

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,55 @@
+import Phaser from 'phaser';
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super();
+        console.log("constructor");
+
+    }
+
+    preload() {
+        console.log("preload");
+    }
+
+    create() {
+        console.log("create");
+
+    }
+
+    update() {
+        console.log("update");
+        this.load.image('background', new URL('../assets/myAssets/myBackground.png',
+            import.meta.url).href);
+        this.load.image('projectile', new URL('../assets/myAssets/myProjectile.png',
+            import.meta.url).href);
+        this.load.image('enemy', new URL('../assets/myAssets/myEnemy.png',
+            import.meta.url).href);
+        this.load.image('player', new URL('../assets/myAssets/myPlayer.png',
+            import.meta.url).href);
+
+    }
+}
+
+// Set configuration for phaser game instance
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 720,
+
+    // Add physics, arcade, and scene
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 0,
+            },
+            debug: false,
+        },
+    },
+    scene: GameScene,
+    audio: {
+        disableWebAudio: true,
+    },
+};
+
+// Initialize game instance
+new Phaser.Game(config);
