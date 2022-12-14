3a206ec9c0d6b39cdf37e45429c7ff5d52e27d91
kikilovespizza <115744484+kikilovespizza@users.noreply.github.com>
2022-12-14 15:36:27 -0500

    final commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,15 @@ class GameScene extends Phaser.Scene {
         // Misc game object declarations
         this.player;
         this.cursors;
+        // Enemy object declaration
+        this.enemies;
+        this.enemySpeed = 150;
+        this.numEnemies = 7;
+        // Game Text declaration
+        this.scoreText;
+        this.score = 0;
+        this.gameOverText;
+        this.gameOver = false;
 
         // Projectile object declaration
         this.projectileImg;
@@ -35,9 +44,29 @@ class GameScene extends Phaser.Scene {
         // Projectile
         this.projectileImg = this.physics.add.sprite(-1440,-1920, 'projectile');
         this.projectileImg.visible = false;
+        // Some enemies for the player to attack
+        this.enemies = this.physics.add.group();
+        this.setEnemies();
+        //  Checks to see if the player collides with any of the enemies, if it does call the onPlayerHitEnemy method
+        this.physics.add.collider(this.player, this.enemies, this.onPlayerHitEnemy, null, this);
+
+        //  Checks to see if the projectile overlaps with any of the enemies, if so call the onProjectileHitEnemy method
+        this.physics.add.overlap(this.projectileImg, this.enemies, this.onProjectileHitEnemy, null, this); 
+        //  The Score text
+        this.scoreText = this.add.text(16, 16, 'Score: 0', {
+            fontSize: '32px',
+            fill: '#ff0000',
+        });
+        // Game over text
+        this.gameOverText = this.add.text(config.width / 2, 400, 'Game Over', {
+            fontSize: '64px',
+            fill: '#000',
+        });
+        this.gameOverText.visible = false;
         // Set world bounds for player
+    
         this.player.setCollideWorldBounds(true);
-        // Fire the projectile
+        
     }
 
     update() { 
@@ -58,6 +87,18 @@ class GameScene extends Phaser.Scene {
         if (this.projectileImg.y <= -16) {
             this.resetProjectile();
         }
+        // On border collision change enemy direction and move down by 64px
+        this.enemies.children.iterate((child) => {
+            const body = child.body;
+            
+            if (body.x < 0) {
+                body.setVelocityX(this.enemySpeed);
+                body.y += 64;
+            }   else if (body.x > 896) {
+                body.setVelocityX(this.enemySpeed * -1);
+                body.y += 64;
+            }
+        });
 
 
 
@@ -83,6 +124,32 @@ class GameScene extends Phaser.Scene {
         this.projectileImg.setVelocityY(0);
         this.projectileImg.visible = false;
     }
+    setEnemies() {
+        for (let i = 0; i < this.numEnemies; i++) {
+            this.enemies.create(Phaser.Math.Between(64, 896), Phaser.Math.Between(64, 296), 'enemy').setScale(.25,.25);
+        }
+        this.enemies.setVelocityX(this.enemySpeed * -1);
+    }
+    onProjectileHitEnemy(projectileImg, enemy) {
+        enemy.disableBody(true, true);
+        projectileImg.body.enable = false;
+        this.resetProjectile();
+        // Increment and update the score
+        this.score += 1;
+        this.scoreText.setText(`Score: ${this.score}`);
+    
+
+    }
+    // Player & Canvas collision
+    onPlayerHitEnemy(player) {
+        this.physics.pause();
+        player.setTint(0xff0000);
+    }
+    //  Game Over
+    showGameOverText() {
+    }
+    
+
 
 }
 
