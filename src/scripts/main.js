67aceaf0ccbd1bc4a65093f674a495f58c59006f
Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
2023-02-07 17:21:46 -0500

    Finished step 09 and added new Images that have transparent backgrounds.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ class GameScene extends Phaser.Scene {
         console.log("constructor");
         this.player;
         this.cursors;
+        this.projectileImg;
+        this.projectileState = 'ready';
     }   
 
     preload() {
@@ -23,8 +25,10 @@ class GameScene extends Phaser.Scene {
         this.player = this.physics.add.sprite(480,600, 'player');
         this.player.setCollideWorldBounds(true);
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.projectileImg = this.physics.add.sprite(-1440, -1920, 'projectile');
+        this.projectileImg.visible = false;
     }
-
+  
     update() {
         console.log("update 1234");
         if (this.cursors.left.isDown) {
@@ -33,7 +37,33 @@ class GameScene extends Phaser.Scene {
         if (this.cursors.right.isDown) {
             this.player.x += 10;
         }
+        if (this.cursors.space.isDown) {
+            if (this.projectileState == 'ready') 
+            {
+                this.fireProjectile();
+            }
+          if (this.projectileImg.y <= -16) {
+              this.resetProjectile();
+          }
+        }
     }
+
+    fireProjectile(){
+        this.projectileState = 'fire';
+        this.projectileImg.visible = true;
+        this.projectileImg.body.enable = true;
+        this.projectileImg.x = this.player.x;
+        this.projectileImg.y = this.player.y;
+        this.projectileImg.setVelocityY(-250); 
+    } 
+    resetProjectile() {
+     if (this.projectileState === 'ready'){
+        return;
+    }
+      this.projectileState = 'ready';
+      this.projectileImg.setVelocityY(0);
+      this.projectileImg.visible = false;
+     }
 }
 
 const config = {
