bb287c498b1bc41b03c848e532fd59c70cd794f8
Mindzeus <71579580+ph4iry@users.noreply.github.com>
2023-03-16 16:56:35 -0400

    moved cell class to new file
    todo: revamp multicell algorithm

---
+++ src/scripts/Cell.js
@@ -0,0 +1,148 @@
+export default class Cell {
+  constructor(x, y, grid, filled) {
+    this.data = { x: x, y: y };
+    this.grid = grid;
+    this.filled = filled || false;
+    this.constants = {
+      LEFT: this.data.x - 1,
+      RIGHT: this.data.x + 1,
+      UP: this.data.y - 1,
+      DOWN: this.data.y + 1,
+    }
+  }
+
+  // TOP THREE CELLS
+  getTopLeftCell() {
+    if (this.constants.LEFT < 0 || this.constants.UP < 0) {
+      return null;
+    } else {
+      return new Cell(this.constants.LEFT, this.constants.UP, this.grid, this.grid[this.constants.UP][this.constants.LEFT].revealed);
+    }
+  }
+
+  getTopCenterCell() {
+    if (this.constants.UP < 0) {
+      return null;
+    } else {
+      return new Cell(this.data.x, this.constants.UP, this.grid, this.grid[this.constants.UP][this.data.x].revealed);
+    }
+  }
+
+  getTopRightCell() {
+    if (this.constants.RIGHT >= this.grid.length || this.constants.UP < 0) {
+      return null;
+    } else {
+      return new Cell(this.constants.RIGHT, this.constants.UP, this.grid, this.grid[this.constants.UP][this.constants.RIGHT].revealed);
+    }
+  }
+
+  // SIDE CENTER CELLS
+  getCenterRightCell() {
+    if (this.constants.RIGHT >= this.grid[0].length) { // Why not use >= 0
+      return null;
+    } else {
+      return new Cell(this.constants.RIGHT, this.data.y, this.grid, this.grid[this.data.y][this.constants.RIGHT].revealed);
+    }
+  }
+
+  getCenterLeftCell() {
+    if (this.constants.LEFT < 0) {
+      return null;
+    } else {
+      return new Cell(this.constants.LEFT, this.data.y, this.grid, this.grid[this.data.y][this.constants.LEFT].revealed);
+    }
+  }
+
+  // BOTTOM THREE CELLS
+  getBottomCenterCell() {
+    if (this.constants.DOWN >= this.grid.length) {
+      return null;
+    } else {
+      return new Cell(this.data.x, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.data.x].revealed);
+    }
+  }
+
+  getBottomRightCell() {
+    if (this.constants.DOWN >= this.grid.length || this.constants.RIGHT >= this.grid[0].length) {
+      return null;
+    } else {
+      return new Cell(this.constants.RIGHT, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.constants.RIGHT].revealed);
+    }
+  }
+
+  getBottomLeftCell() {
+    if (this.constants.DOWN >= this.grid.length || this.constants.LEFT < 0) {
+      return null;
+    } else {
+      return new Cell(this.constants.LEFT, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.constants.LEFT].revealed);
+    }
+  }
+
+  // Surrounding cells compiled into one object for usage
+  /**
+   * 
+   * @returns An object containing all of the surrounding cells. If a value is null, it exceeds the bounds of the grid.
+   */
+  getSurroundingCells() {
+    const surroundingCells = {
+      topLeft: this.getTopLeftCell(),
+      topCenter: this.getTopCenterCell(),
+      topRight: this.getTopRightCell(),
+      centerRight: this.getCenterRightCell(),
+      bottomRight: this.getBottomRightCell(),
+      bottomCenter: this.getBottomCenterCell(),
+      bottomLeft: this.getBottomLeftCell(),
+      centerLeft: this.getCenterLeftCell(),
+    }
+    return Object.assign({ asArray: Object.values(surroundingCells) }, surroundingCells)
+  }
+
+  /**
+   * 
+   * @returns An object containing all of the vertically and horizontally adjacent cells in a "t" shape.
+   */
+  getTCells() {
+    const tCells = {
+      topCenter: this.getTopCenterCell(),
+      centerRight: this.getCenterRightCell(),
+      bottomCenter: this.getBottomCenterCell(),
+      centerLeft: this.getCenterLeftCell(),
+    }
+
+    return Object.assign({ asArray: Object.values(tCells) }, tCells);
+  }
+
+  /**
+   * 
+   * @returns An object containing all of the diagonally adjacent cells in an "x" shape.
+   */
+  getXCells() {
+    const xCells = {
+      topLeft: this.getTopLeftCell(),
+      topRight: this.getTopRightCell(),
+      bottomRight: this.getBottomRightCell(),
+      bottomLeft: this.getBottomLeftCell(),
+    }
+    return Object.assign({ asArray: Object.values(xCells) }, xCells)
+  }
+
+  countUnpaintedCells() {
+    return this.getSurroundingCells().asArray.filter(cell => cell !== null && !cell.filled).length;
+  }
+
+  /**
+   * 
+   * @param {'all' | 'x' | 't'} direction 
+   * @returns A random cell adjacent in the given direction.
+   */
+
+  getRandomCell(direction) {
+    const directions = {
+      all: 'getSurroundingCells',
+      x: 'getXCells',
+      t: 'getTCells'
+    }
+    const validCells = this[directions['direction'] || 'getSurroundingCells']().asArray.filter(cell => cell !== null && cell.filled !== true);
+    return validCells[Math.floor(Math.random() * validCells.length)];
+  }
+}
--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -1,167 +1,19 @@
 import Phaser from 'phaser';
-import AnimationScene from './AnimationScene'
+import AnimationScene from './AnimationScene';
+import Cell from './Cell';
 
 const tiers = [50, 100, 250, 500, 1000, 2500, 5000];
 
-class Cell {
-  constructor(x, y, grid, filled) {
-    this.data = { x: x, y: y };
-    this.grid = grid;
-    this.filled = filled || false;
-    this.constants = {
-      LEFT: this.data.x - 1,
-      RIGHT: this.data.x + 1,
-      UP: this.data.y - 1,
-      DOWN: this.data.y + 1,
-    }
-  }
+// class ExpanderCell extends Cell {
+//   constructor(x, y, grid, filled, parent) {
+//     super(x, y, grid, filled);
+//     this.parentCell = parent;
+//   }
 
-  // TOP THREE CELLS
-  getTopLeftCell() {
-    if (this.constants.LEFT < 0 || this.constants.UP < 0) {
-      return null;
-    } else {
-      return new Cell(this.constants.LEFT, this.constants.UP, this.grid, this.grid[this.constants.UP][this.constants.LEFT].revealed);
-    }
-  }
-
-  getTopCenterCell() {
-    if (this.constants.UP < 0) {
-      return null;
-    } else {
-      return new Cell(this.data.x, this.constants.UP, this.grid, this.grid[this.constants.UP][this.data.x].revealed);
-    }
-  }
-
-  getTopRightCell() {
-    if (this.constants.RIGHT >= this.grid.length || this.constants.UP < 0) {
-      return null;
-    } else {
-      return new Cell(this.constants.RIGHT, this.constants.UP, this.grid, this.grid[this.constants.UP][this.constants.RIGHT].revealed);
-    }
-  }
-
-  // SIDE CENTER CELLS
-  getCenterRightCell() {
-    if (this.constants.RIGHT >= this.grid[0].length) { // Why not use >= 0
-      return null;
-    } else {
-      return new Cell(this.constants.RIGHT, this.data.y, this.grid, this.grid[this.data.y][this.constants.RIGHT].revealed);
-    }
-  }
-
-  getCenterLeftCell() {
-    if (this.constants.LEFT < 0) {
-      return null;
-    } else {
-      return new Cell(this.constants.LEFT, this.data.y, this.grid, this.grid[this.data.y][this.constants.LEFT].revealed);
-    }
-  }
-
-  // BOTTOM THREE CELLS
-  getBottomCenterCell() {
-    if (this.constants.DOWN >= this.grid.length) {
-      return null;
-    } else {
-      return new Cell(this.data.x, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.data.x].revealed);
-    }
-  }
-
-  getBottomRightCell() {
-    if (this.constants.DOWN >= this.grid.length || this.constants.RIGHT >= this.grid[0].length) {
-      return null;
-    } else {
-      return new Cell(this.constants.RIGHT, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.constants.RIGHT].revealed);
-    }
-  }
-
-  getBottomLeftCell() {
-    if (this.constants.DOWN >= this.grid.length || this.constants.LEFT < 0) {
-      return null;
-    } else {
-      return new Cell(this.constants.LEFT, this.constants.DOWN, this.grid, this.grid[this.constants.DOWN][this.constants.LEFT].revealed);
-    }
-  }
-
-  // Surrounding cells compiled into one object for usage
-  /**
-   * 
-   * @returns An object containing all of the surrounding cells. If a value is null, it exceeds the bounds of the grid.
-   */
-  getSurroundingCells() {
-    const surroundingCells = {
-      topLeft: this.getTopLeftCell(),
-      topCenter: this.getTopCenterCell(),
-      topRight: this.getTopRightCell(),
-      centerRight: this.getCenterRightCell(),
-      bottomRight: this.getBottomRightCell(),
-      bottomCenter: this.getBottomCenterCell(),
-      bottomLeft: this.getBottomLeftCell(),
-      centerLeft: this.getCenterLeftCell(),
-    }
-    return Object.assign({ asArray: Object.values(surroundingCells) }, surroundingCells)
-  }
-
-  /**
-   * 
-   * @returns An object containing all of the vertically and horizontally adjacent cells in a "t" shape.
-   */
-  getTCells() {
-    const tCells = {
-      topCenter: this.getTopCenterCell(),
-      centerRight: this.getCenterRightCell(),
-      bottomCenter: this.getBottomCenterCell(),
-      centerLeft: this.getCenterLeftCell(),
-    }
-
-    return Object.assign({ asArray: Object.values(tCells) }, tCells);
-  }
-
-  /**
-   * 
-   * @returns An object containing all of the diagonally adjacent cells in an "x" shape.
-   */
-  getXCells() {
-    const xCells = {
-      topLeft: this.getTopLeftCell(),
-      topRight: this.getTopRightCell(),
-      bottomRight: this.getBottomRightCell(),
-      bottomLeft: this.getBottomLeftCell(),
-    }
-    return Object.assign({ asArray: Object.values(xCells) }, xCells)
-  }
-
-  countUnpaintedCells() {
-    return this.getSurroundingCells().asArray.filter(cell => cell !== null && !cell.filled).length;
-  }
-
-  /**
-   * 
-   * @param {'all' | 'x' | 't'} direction 
-   * @returns A random cell adjacent in the given direction.
-   */
-
-  getRandomCell(direction) {
-    const directions = {
-      all: 'getSurroundingCells',
-      x: 'getXCells',
-      t: 'getTCells'
-    }
-    const validCells = this[directions['direction'] || 'getSurroundingCells']().asArray.filter(cell => cell !== null && cell.filled !== true);
-    return validCells[Math.floor(Math.random() * validCells.length)];
-  }
-}
-
-class ExpanderCell extends Cell {
-  constructor(x, y, grid, filled, parent) {
-    super(x, y, grid, filled);
-    this.parentCell = parent;
-  }
-
-  expandFromParent(fn) {
-    fn()
-  }
-}
+//   expandFromParent(fn) {
+//     fn()
+//   }
+// }
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -230,13 +82,10 @@ class GameScene extends Phaser.Scene {
   }
 
   update() {
-    if (this.input.keyboard.checkDown(this.controls['ONE'], 1000)) {
+    if (this.input.keyboard.checkDown(this.controls['ONE'], 10)) {
       this.fillSquares(tiers[0], this.rows, this.items)
-
-
-
     }
-    if (this.input.keyboard.checkDown(this.controls['TWO'], 0)) {
+    if (this.input.keyboard.checkDown(this.controls['TWO'], 100)) {
       this.fillSquares(tiers[1], this.rows, this.items)
     }
     if (this.input.keyboard.checkDown(this.controls['THREE'], 1000)) {
@@ -256,50 +105,79 @@ class GameScene extends Phaser.Scene {
     }
   }
 
-  findEmptyCell(rows) {
+  fillSquare(x, y, rows, items) {
 
+  }
+
+  checkForCompletion(rows) {
+    return rows.every((row) => {
+      return row.every(item => item.revealed)
+    });
   }
 
   fillSquares(donation, rows, items) {
     const squares = donation / 50;
     // if (squares < 10) {
-
     // debug edge case: already filled cells are being checked
     let src = rows[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 50)];
-    console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
-    let count = 0
+    // console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
+    let count = 0;
     while (src.revealed) {
-      src = rows[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 50)];
-      count++;
-      if (count > 2000) {
+      if (this.checkForCompletion(rows)) {
         console.log("All Cells Revealed!!!")
         break;
       }
+      src = rows[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 50)];
+      count++;
       console.log(count)
     }
-    console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
-    // console.log("Rows: " + rows[src.y][src.x].y);
-    console.log("Rows: " + rows[src.y][src.x].x + ", " + rows[src.y][src.x].y + ", " + rows[src.y][src.x].revealed);
+
+
+    // console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
+    // // console.log("Rows: " + rows[src.y][src.x].y);
+    // console.log("Rows: " + rows[src.y][src.x].x + ", " + rows[src.y][src.x].y + ", " + rows[src.y][src.x].revealed);
 
     src.revealed = true;
     const centerCell = new Cell(src.x, src.y, rows, src.revealed);
     items.getChildren().find(item => item.name === `(${centerCell.data.x}, ${centerCell.data.y})`).setVisible(false);
-    let total = 1;
-    while (total < squares) {
-      const targetCell = centerCell.getRandomCell();
-      if (!targetCell.filled) {
-        targetCell.filled = true;
-        items.getChildren().find(i => i.name === `(${targetCell.data.x}, ${targetCell.data.y})`).setVisible(false);
-        rows[targetCell.data.y][targetCell.data.x].revealed = true;
-        total++;
-        // console.log(total, targetCell.filled, targetCell.data, centerCell.getSurroundingCells().asArray.map(c => `${c.data.x}, ${c.data.y}`));
-      } else {
-        continue;
-      }
-      if (centerCell.getSurroundingCells().asArray.filter(cell => cell !== null && !cell.filled).length < 1) {
-        break;
-      }
-    }
+    // let total = 1;
+
+    // while (total < squares) {
+    //   let src = rows[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 50)];
+    //   console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
+    //   let count = 0
+    //   while (src.revealed) {
+    //     src = rows[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 50)];
+    //     count++;
+    //     if (count > 2000) {
+    //       console.log("All Cells Revealed!!!")
+    //       break;
+    //     }
+    //     console.log(count)
+    //   }
+    //   // console.log("Src: " + src.x + ", " + src.y + ", " + src.revealed);
+    //   // // console.log("Rows: " + rows[src.y][src.x].y);
+    //   // console.log("Rows: " + rows[src.y][src.x].x + ", " + rows[src.y][src.x].y + ", " + rows[src.y][src.x].revealed);
+  
+    //   src.revealed = true;
+    //   const centerCell = new Cell(src.x, src.y, rows, src.revealed);
+    //   items.getChildren().find(item => item.name === `(${centerCell.data.x}, ${centerCell.data.y})`).setVisible(false);
+      
+    //   const targetCell = centerCell.getRandomCell();
+    //   if (!targetCell.filled) {
+    //     targetCell.filled = true;
+    //     items.getChildren().find(i => i.name === `(${targetCell.data.x}, ${targetCell.data.y})`).setVisible(false);
+    //     rows[targetCell.data.y][targetCell.data.x].revealed = true;
+    //     total++;
+    //     // console.log(total, targetCell.filled, targetCell.data, centerCell.getSurroundingCells().asArray.map(c => `${c.data.x}, ${c.data.y}`));
+    //   } else {
+    //     continue;
+    //   }
+    //   if (centerCell.getSurroundingCells().asArray.filter(cell => cell !== null && !cell.filled).length < 1) {
+    //     break;
+    //   }
+    // }
+
     // centerCell.getSurroundingCells().asArray.forEach(surroundingCell => {
     //   if (total < squares && surroundingCell !== null) {
     //     surroundingCell.filled = true;
@@ -308,23 +186,24 @@ class GameScene extends Phaser.Scene {
     //     total++;
     //   }
     // });
-    if (total < squares) {
-      const expandables = centerCell.getSurroundingCells().asArray.filter(cell => cell !== null && cell.countUnpaintedCells() > 0);
-      // console.log(expandables);
-      if (expandables.length > 0) {
-        const expanderCell = expandables[Math.floor(Math.random() * expandables.length)];
-        // console.log(expanderCell);
-        this.expandSquares(expanderCell, total, squares, items, rows);
-        // while (total < squares) {
-        //   const nextCell = expanderCell.getRandomCell();
-        //   if (!nextCell || !nextCell.filled === true) continue;
-        //   nextCell.filled = true;
-        //   items.getChildren().find(i => i.name === `(${nextCell.data.x}, ${nextCell.data.y})`).setVisible(false);
-        //   rows[nextCell.data.y][nextCell.data.x].revealed = true;s
-        //   total++;
-        // }
-      }
-    }
+
+    // if (total < squares) {
+    //   // const expandables = centerCell.getSurroundingCells().asArray.filter(cell => cell !== null && cell.countUnpaintedCells() > 0);
+    //   // console.log(expandables);
+    //   // if (expandables.length > 0) {
+    //   //   const expanderCell = expandables[Math.floor(Math.random() * expandables.length)];
+    //   //   // console.log(expanderCell);
+    //   //   this.expandSquares(expanderCell, total, squares, items, rows);
+    //   //   // while (total < squares) {
+    //   //   //   const nextCell = expanderCell.getRandomCell();
+    //   //   //   if (!nextCell || !nextCell.filled === true) continue;
+    //   //   //   nextCell.filled = true;
+    //   //   //   items.getChildren().find(i => i.name === `(${nextCell.data.x}, ${nextCell.data.y})`).setVisible(false);
+    //   //   //   rows[nextCell.data.y][nextCell.data.x].revealed = true;s
+    //   //   //   total++;
+    //   //   // }
+    //   // }
+    // }
     // }
   }
 
