12a6cf672a70bbb8bb35b88524ce1e10d0595c1c
sionhur <gloria7hur@gmail.com>
2023-11-07 09:21:45 -0500

    updated

---
+++ src/scripts/main.js
@@ -0,0 +1,42 @@
+import Phaser from 'phaser';
+
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super();
+        console.log("constructor");
+    }
+    preload() {
+        console.log("preload");
+    }
+    create() {
+        console.log("create");
+    }
+    update() {
+        console.log("update");
+    }
+}
+
+// Set configuration for phaser game instance
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 720,
+
+    // Add physics, arcade, and scene
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 0,
+            },
+            debug: false,
+        },
+    },
+    scene: GameScene,
+    audio: {
+        disableWebAudio: true,
+    },
+};
+
+// Initialize game instance
+new Phaser.Game(config);
